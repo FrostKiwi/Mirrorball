47fa87c3e582ac7484c44b4edbab62a3381385d3
Wladislav ヴラド Artsimovich <wladislav.artsimovich@dmgmori.com>
2023-06-07 18:19:23 +0900

    Seperate and style the WASM skeleton

--- Makefile
+++ Makefile
@@ -3,6 +3,10 @@ SRC = $(wildcard src/*.c)
 release: CFLAGS = -Wall -O3 -flto # O3 + LTO
 debug: CFLAGS = -Wall -O0
 OBJ = $(addprefix obj/, $(notdir $(SRC:.c=.o)))
+# Functions to be prevented from being deleted as dead code. This is to allow
+# for C code to be alled from Javascript, even when the functions in question
+# are not called in the C code itself. Other options is EMSCRIPTEN_KEEPALIVE,
+# but that doesn't for for things not defined in the C source like malloc.
 PROTECT = ["_load_photo", "_main", "_malloc", "_free"]
 
 # List resource directories manually, instead of a recursive find to prevent
@@ -16,6 +20,9 @@ CAUSE_FOR_RECOMPILE = $(wildcard inc/*.h) \
 					  $(wildcard src/web/*) \
 					  Makefile
 
+# These emscripten flags need to be present during compilation, because this
+# triggers the package manager to download everything that's needed like SDL and
+# the associated dependencies.
 EMCC_FLAGS= -s USE_SDL=2 \
 			-s USE_SDL_IMAGE=2 \
 			-s SDL2_IMAGE_FORMATS='["jpg", "png", "heic"]'
@@ -30,6 +37,7 @@ EMCC_LINKER_FLAGS = $(EMCC_FLAGS) \
 
 release debug: out/index.html $(OBJ)
 
+# Linking Stage + Output to WASM.
 out/index.html: $(OBJ)
 	emcc $(EMCC_LINKER_FLAGS) \
 		 -Iinc $(OBJ) $(CFLAGS) \
@@ -37,6 +45,7 @@ out/index.html: $(OBJ)
 		 --shell-file src/web/shell.html
 	cp src/web/* out
 
+# Compile every C file in parallel.
 # Changes to the headers, Makefile or shaders as cause to recompile everything
 obj/%.o: src/%.c $(CAUSE_FOR_RECOMPILE)
 	mkdir -p obj
---
+++ src/web/shell.js
@@ -0,0 +1,87 @@
+var open_file = function (e) {
+    const file_reader = new FileReader();
+    file_reader.onload = (event) => {
+        const uint8Arr = new Uint8Array(event.target.result);
+        const num_bytes = uint8Arr.length * uint8Arr.BYTES_PER_ELEMENT;
+        const data_ptr = Module._malloc(num_bytes);
+        const data_on_heap = new Uint8Array(Module.HEAPU8.buffer, data_ptr, num_bytes);
+        data_on_heap.set(uint8Arr);
+        const res = Module.ccall('load_photo', 'number', ['number', 'number'], [data_on_heap.byteOffset, uint8Arr.length]);
+        Module._free(data_ptr);
+    };
+    file_reader.readAsArrayBuffer(e.target.files[0]);
+};
+
+var statusElement = document.getElementById('status');
+var progressElement = document.getElementById('progress');
+
+var Module = {
+    preRun: [],
+    postRun: [],
+    print: (function () {
+        var element = document.getElementById('output');
+        if (element) element.value = ''; // clear browser cache
+        return function (text) {
+            if (arguments.length > 1) text = Array.prototype.slice.call(arguments).join(' ');
+            console.log(text);
+            if (element) {
+                element.value += text + "\n";
+                element.scrollTop = element.scrollHeight; // focus on bottom
+            }
+        };
+    })(),
+    canvas: (function () {
+        let viewportmeta = document.querySelector('meta[name="viewport"]');
+        if (viewportmeta === null) {
+            viewportmeta = document.createElement("meta");
+            viewportmeta.setAttribute("name", "viewport");
+            document.head.appendChild(viewportmeta);
+
+            viewportmeta = document.querySelector('meta[name="viewport"]');
+        }
+        /* viewportmeta.setAttribute('content', "initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"); */
+
+        var canvas = document.getElementById('canvas');
+
+        // As a default initial behavior, pop up an alert when webgl context is lost. To make your
+        // application robust, you may want to override this behavior before shipping!
+        // See http://www.khronos.org/registry/webgl/specs/latest/1.0/#5.15.2
+        canvas.addEventListener("webglcontextlost", function (e) { alert('WebGL context lost. You will need to reload the page.'); e.preventDefault(); }, false);
+
+        return canvas;
+    })(),
+    setStatus: function (text) {
+        if (!Module.setStatus.last) Module.setStatus.last = { time: Date.now(), text: '' };
+        if (text === Module.setStatus.last.text) return;
+        var m = text.match(/([^(]+)\((\d+(\.\d+)?)\/(\d+)\)/);
+        var now = Date.now();
+        if (m && now - Module.setStatus.last.time < 30) return; // if this is a progress update, skip it if too soon
+        Module.setStatus.last.time = now;
+        Module.setStatus.last.text = text;
+        if (m) {
+            text = m[1];
+            progressElement.value = parseInt(m[2]) * 100;
+            progressElement.max = parseInt(m[4]) * 100;
+            progressElement.hidden = false;
+        } else {
+            progressElement.value = null;
+            progressElement.max = null;
+            progressElement.hidden = true;
+        }
+        statusElement.innerHTML = text;
+    },
+    totalDependencies: 0,
+    monitorRunDependencies: function (left) {
+        this.totalDependencies = Math.max(this.totalDependencies, left);
+        Module.setStatus(left ? 'Preparing... (' + (this.totalDependencies - left) + '/' + this.totalDependencies + ')' : 'All downloads complete.');
+    }
+};
+
+Module.setStatus('Downloading...');
+
+window.onerror = function () {
+    Module.setStatus('Exception thrown, see JavaScript console');
+    Module.setStatus = function (text) {
+        if (text) console.error('[post-exception status] ' + text);
+    };
+};
