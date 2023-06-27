2cc314362c1e152244346be67da5d19043f63786
Wladislav ヴラド Artsimovich <git@frost.kiwi>
2023-06-27 11:08:23 +0900

    Proper canvas resizing

--- main.js
+++ main.js
@@ -4,14 +4,10 @@ import { GUI } from 'dat.gui';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-	75, window.innerWidth / window.innerHeight, 0.1, 1000
+	75, 2, 0.1, 1000
 );
 
-window.addEventListener( 'resize', onWindowResize, false );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+const renderer = new THREE.WebGLRenderer( { canvas: document.querySelector( "canvas" ) } );
 
 const stats = new Stats();
 document.body.appendChild( stats.dom );
@@ -33,8 +29,27 @@ settingsFolder.add( showStats, 'show' ).onChange( function ( value ) {
 
 } );
 
+
+
+function resizeCanvasToDisplaySize() {
+
+	const canvas = renderer.domElement;
+	const width = canvas.clientWidth;
+	const height = canvas.clientHeight;
+
+	if ( canvas.width !== width || canvas.height !== height ) {
+
+		renderer.setSize( width, height, false );
+		camera.aspect = width / height;
+		camera.updateProjectionMatrix();
+
+	}
+
+}
+
 function animate() {
 
+	resizeCanvasToDisplaySize();
 	requestAnimationFrame( animate );
 
 	render();
@@ -43,19 +58,10 @@ function animate() {
 
 }
 
-animate();
+requestAnimationFrame( animate );
 
 function render() {
 
 	renderer.render( scene, camera );
-
-}
-
-function onWindowResize() {
-
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-
-	renderer.setSize( window.innerWidth, window.innerHeight );
 
 }
