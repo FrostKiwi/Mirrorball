da8bcb9a8e45d8cf36b6edb627804ff74db48094
Wladislav ヴラド Artsimovich <git@frost.kiwi>
2023-06-27 18:56:47 +0900

    Proper resizing added

--- main.js
+++ main.js
@@ -51,15 +51,13 @@ async function init() {
 function animate() {
 
 	requestAnimationFrame( animate );
-	webglUtils.resizeCanvasToDisplaySize( canvas, window.devicePixelRatio );
+	resizeCanvasToDisplaySize( canvas );
 
 	render();
 
 	ctx.stats.update();
 
 }
-
-main();
 
 function render() {
 
@@ -216,3 +214,79 @@ function media_setup( bitmap ) {
 	bitmap.close();
 
 }
+
+const canvasToDisplaySizeMap = new Map( [[ canvas, [ 300, 150 ]]] );
+
+function onResize( entries ) {
+
+	for ( const entry of entries ) {
+
+		let width;
+		let height;
+		let dpr = window.devicePixelRatio;
+		if ( entry.devicePixelContentBoxSize ) {
+
+			// NOTE: Only this path gives the correct answer
+			// The other 2 paths are an imperfect fallback
+			// for browsers that don't provide anyway to do this
+			width = entry.devicePixelContentBoxSize[ 0 ].inlineSize;
+			height = entry.devicePixelContentBoxSize[ 0 ].blockSize;
+			dpr = 1; // it's already in width and height
+
+		} else if ( entry.contentBoxSize ) {
+
+			if ( entry.contentBoxSize[ 0 ] ) {
+
+				width = entry.contentBoxSize[ 0 ].inlineSize;
+				height = entry.contentBoxSize[ 0 ].blockSize;
+
+			} else {
+
+				// legacy
+				width = entry.contentBoxSize.inlineSize;
+				height = entry.contentBoxSize.blockSize;
+
+			}
+
+		} else {
+
+			// legacy
+			width = entry.contentRect.width;
+			height = entry.contentRect.height;
+
+		}
+
+		const displayWidth = Math.round( width * dpr );
+		const displayHeight = Math.round( height * dpr );
+		canvasToDisplaySizeMap.set( entry.target, [ displayWidth, displayHeight ] );
+
+	}
+
+}
+
+const resizeObserver = new ResizeObserver( onResize );
+resizeObserver.observe( canvas, { box: 'content-box' } );
+
+function resizeCanvasToDisplaySize( canvas ) {
+
+	// Get the size the browser is displaying the canvas in device pixels.
+	const [ displayWidth, displayHeight ] = canvasToDisplaySizeMap.get( canvas );
+
+	// Check if the canvas is not the same size.
+	const needResize = canvas.width !== displayWidth ||
+                       canvas.height !== displayHeight;
+
+	if ( needResize ) {
+
+		// Make the canvas the same size
+		canvas.width = displayWidth;
+		canvas.height = displayHeight;
+
+	}
+
+	return needResize;
+
+}
+
+
+main();
