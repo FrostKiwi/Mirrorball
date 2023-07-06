9501948306c996f1bfd39b42ac25a6b6234616b0
Wladislav ヴラド Artsimovich <git@frost.kiwi>
2023-07-06 13:54:38 +0900

    Render loop in separate file

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,7 @@ import ctx from './state.js';
 import init_gui from './gui.js';
 import { resizeCanvasToDisplaySize, onResize } from './resize.js'
 import init_shaders from './init_shaders.js'
-import render_crop from './render_crop.js'
-import render_project from './render_projection.js'
-import update_camera from './update_camera.js'
-import render_border from './render_border.js'
+import render from './render.js'
 import { key_input, setup_input } from './input.js'
 
 ctx.canvas = document.querySelector("canvas");
@@ -63,52 +60,6 @@ function animate(time) {
 	render();
 
 	ctx.stats.update();
-}
-
-function render() {
-	if (!ctx.gui.crop && !ctx.gui.project)
-		ctx.gl.clear(ctx.gl.COLOR_BUFFER_BIT);
-	ctx.gl.viewport(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-	/* Devision tree of all combinations of splitscreen rendering and point
-	   visualization */
-	if (ctx.gui.crop && ctx.gui.project) {
-		if (ctx.canvas.width / ctx.canvas.height > 1) {
-			update_camera(ctx.canvas.width / 2, ctx.canvas.height);
-			render_crop(ctx.canvas.width / 2, ctx.canvas.height);
-			render_project(ctx.canvas.width / 2, ctx.canvas.height);
-			if (ctx.gui.viz) {
-				render_border(true, ctx.gui.viz_subdiv,
-					ctx.canvas.width / 2, ctx.canvas.height);
-				render_border(false, ctx.gui.viz_subdiv,
-					ctx.canvas.width / 2, ctx.canvas.height);
-			}
-		} else {
-			update_camera(ctx.canvas.width, ctx.canvas.height / 2);
-			render_crop(ctx.canvas.width, ctx.canvas.height / 2);
-			render_project(ctx.canvas.width, ctx.canvas.height / 2);
-			if (ctx.gui.viz) {
-				render_border(true, ctx.gui.viz_subdiv,
-					ctx.canvas.width, ctx.canvas.height / 2);
-				render_border(false, ctx.gui.viz_subdiv,
-					ctx.canvas.width, ctx.canvas.height / 2);
-			}
-		}
-	} else {
-		update_camera(ctx.canvas.width, ctx.canvas.height);
-		if (ctx.gui.crop) {
-			render_crop(ctx.canvas.width, ctx.canvas.height);
-			if (ctx.gui.viz)
-				render_border(true, ctx.gui.viz_subdiv,
-					ctx.canvas.width, ctx.canvas.height);
-		}
-		if (ctx.gui.project) {
-			render_project();
-			if (ctx.gui.viz)
-				render_border(false, ctx.gui.viz_subdiv,
-					ctx.canvas.width, ctx.canvas.height);
-		}
-	}
 }
 
 async function load_from_url(url) {
