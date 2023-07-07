8210334854a06408217911e44bbba7ae6fda4bb3
Wladislav ヴラド Artsimovich <git@frost.kiwi>
2023-07-07 17:24:53 +0900

    Redraw also on gui actions

--- src/js/gui.js
+++ src/js/gui.js
@@ -1,27 +1,31 @@
-import { ctx } from './state.js';
+import { ctx, redraw } from './state.js';
 import GUI from 'lil-gui';
 
 export default function init_gui() {
 	ctx.gui.handle = new GUI().hide();
 	ctx.gui.handle.title("Controls (Show / Hide)");
 	ctx.gui.handle.add(ctx.gui, 'menu').name("Toggle Main Menu");
-	ctx.gui.handle.add(ctx.gui, 'crop').name("Original");
-	ctx.gui.handle.add(ctx.gui, 'project').name("Projection");
+	ctx.gui.handle.add(ctx.gui, 'crop').name("Original").onChange(redraw);
+	ctx.gui.handle.add(ctx.gui, 'project').name("Projection").onChange(redraw);
 
 	/* Visualizations */
 	ctx.gui.folder.viz = ctx.gui.handle.addFolder('Visualizations').open();
-	ctx.gui.folder.viz.add(ctx.shaders.crop, 'mask').name("Circle Mask");
-	ctx.gui.folder.viz.add(ctx.gui, 'viz').name("Project screen border");
+	ctx.gui.folder.viz.add(ctx.shaders.crop, 'mask').name(
+		"Circle Mask"
+	).onChange(redraw);
+	ctx.gui.folder.viz.add(ctx.gui, 'viz').name(
+		"Project screen border"
+	).onChange(redraw);
 
 	ctx.gui.folder.camera = ctx.gui.handle.addFolder('Camera').close();
 	ctx.gui.controller.cam_fov = ctx.gui.folder.camera.add(
 		ctx.cam.fov, 'cur', ctx.cam.fov.min, ctx.cam.fov.max, 1).name(
 			"Vertical FOV [in °]"
-		);
+		).onChange(redraw);
 	ctx.gui.controller.pitch = ctx.gui.folder.camera.add(
-		ctx.cam.rot_deg, '0', -90, 90).name("Pitch [in °]");
+		ctx.cam.rot_deg, '0', -90, 90).name("Pitch [in °]").onChange(redraw);
 	ctx.gui.controller.yaw = ctx.gui.folder.camera.add(
-		ctx.cam.rot_deg, '1', -180, 180).name("Yaw [in °]");
+		ctx.cam.rot_deg, '1', -180, 180).name("Yaw [in °]").onChange(redraw);
 
 	/* Projection Controls */
 	ctx.gui.folder.setup =
@@ -29,22 +33,24 @@ export default function init_gui() {
 
 	/* Sphere's FOV / Distortion correction */
 	ctx.gui.controller.img_fov =
-		ctx.gui.folder.setup.add(ctx.ch1, 'fov_deg', 180, 360, 1);
-	ctx.gui.controller.img_fov.name("Sphere's FOV [in °]");
+		ctx.gui.folder.setup.add(ctx.ch1, 'fov_deg', 180, 360, 1).name(
+			"Sphere's FOV [in °]"
+	).onChange(redraw);
 
 	/* Crop */
 	ctx.gui.folder.crop = ctx.gui.folder.setup.addFolder('Image crop');
 	ctx.gui.controller.top =
-		ctx.gui.folder.crop.add(ctx.ch1.crop, 'top', 0, 1, 1);
+		ctx.gui.folder.crop.add(ctx.ch1.crop, 'top', 0, 1, 1).onChange(redraw);
 	ctx.gui.controller.top.name("Top [px]");
 	ctx.gui.controller.bot =
-		ctx.gui.folder.crop.add(ctx.ch1.crop, 'bot', 0, 1, 1);
+		ctx.gui.folder.crop.add(ctx.ch1.crop, 'bot', 0, 1, 1).onChange(redraw);
 	ctx.gui.controller.bot.name("Bottom [px]");
 	ctx.gui.controller.left =
-		ctx.gui.folder.crop.add(ctx.ch1.crop, 'left', 0, 1, 1);
+		ctx.gui.folder.crop.add(ctx.ch1.crop, 'left', 0, 1, 1).onChange(redraw);
 	ctx.gui.controller.left.name("Left [px]");
 	ctx.gui.controller.right =
-		ctx.gui.folder.crop.add(ctx.ch1.crop, 'right', 0, 1, 1);
+		ctx.gui.folder.crop.add(
+			ctx.ch1.crop, 'right', 0, 1, 1).onChange(redraw);
 	ctx.gui.controller.right.name("Right [px]");
 	ctx.gui.folder.crop.add(ctx.gui, 'crop_negative').onChange(
 		toggle_crop_negative).name("Allow negative");
@@ -53,13 +59,13 @@ export default function init_gui() {
 	ctx.gui.folder.world = ctx.gui.folder.setup.addFolder('World Rotation');
 	ctx.gui.folder.world.add(ctx.ch1.rot_deg, '0', -180, 180).name(
 		"Pitch [in °]"
-	);
+	).onChange(redraw);
 	ctx.gui.folder.world.add(ctx.ch1.rot_deg, '1', -180, 180).name(
 		"Yaw [in °]"
-	);
+	).onChange(redraw);
 	ctx.gui.folder.world.add(ctx.ch1.rot_deg, '2', -180, 180).name(
 		"Roll [in °]"
-	);
+	).onChange(redraw);
 
 	ctx.gui.folder.settings = ctx.gui.handle.addFolder('Settings').close();
 
