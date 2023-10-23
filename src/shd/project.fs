cc2a623e08dad2b6c90187109e09771204312cb6
Wladislav ヴラド Artsimovich <git@frost.kiwi>
2023-10-23 14:37:44 +0900

    Mostly finished AA implementation

--- src/shd/project.fs
+++ src/shd/project.fs
@@ -18,28 +18,30 @@ uniform float alpha;
 
 void main()
 {
-    vec3 R = normalize(Ray);
-    vec2 dist = scalar * R.xy / (M_2xSQRT2 * sqrt(R.z + 1.0));
+	vec3 R = normalize(Ray);
+	vec2 dist = scalar * R.xy / (M_2xSQRT2 * sqrt(R.z + 1.0));
 
-    float blind_spot = length(dist) - 0.5;
-    float smoothedAlpha = clamp(0.5 - blind_spot / (fwidth(blind_spot)), 0.0, 1.0);
+	float blind_spot = length(dist) - 0.5;
+	float smoothedAlpha = clamp(0.5 - blind_spot / (fwidth(blind_spot)), 0.0, 1.0);
 
-    vec2 uv = dist * vec2(crop.z, crop.w);
-    uv.x = crop.x + uv.x;
-    uv.y = crop.y - uv.y;
+	vec2 uv = dist * vec2(crop.z, crop.w);
+	uv.x = crop.x + uv.x;
+	uv.y = crop.y - uv.y;
 
-    vec4 baseColor = vec4(texture2D(sample_projection, uv).rgb, alpha);
-    vec4 greenColor = baseColor * vec4(0.5, 1, 0.5, alpha);
-    vec4 redColor = baseColor * vec4(1, 0.5, 0.5, alpha);
-    vec4 blackColor = vec4(0.0, 0.0, 0.0, alpha);
+	vec4 baseColor = vec4(texture2D(sample_projection, uv).rgb, alpha);
+	vec4 greenColor = baseColor * vec4(0.5, 1, 0.5, alpha);
+	vec4 redColor = baseColor * vec4(1, 0.5, 0.5, alpha);
+	vec4 blackColor = vec4(0.0, 0.0, 0.0, alpha);
 
-    float edgeWidth = 0.0001;
-    float factorGreen = clamp((area_f - length(dist * scalar_rcp)) / edgeWidth, 0.0, 1.0);
-    float factorRed = clamp((length(dist * scalar_rcp) - area_b) / edgeWidth, 0.0, 1.0);
-    float factorBlack = 1.0 - smoothedAlpha;
+	// Using division-based method for antialiasing
+	float lenDist = length(dist * scalar_rcp);
 
-    gl_FragColor = baseColor;
-    gl_FragColor = mix(gl_FragColor, greenColor, factorGreen);
-    gl_FragColor = mix(gl_FragColor, redColor, factorRed);
-    gl_FragColor = mix(gl_FragColor, blackColor, factorBlack);
+	float factorGreen = clamp((area_f - lenDist) / fwidth(lenDist), 0.0, 1.0);
+	float factorRed = clamp((lenDist - area_b) / fwidth(lenDist), 0.0, 1.0);
+	float factorBlack = 1.0 - smoothedAlpha;
+
+	gl_FragColor = baseColor * (1.0 - factorGreen - factorRed - factorBlack) +
+				   greenColor * factorGreen +
+				   redColor * factorRed +
+				   blackColor * factorBlack;
 }
