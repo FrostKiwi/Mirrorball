8fed8f23771f2eaea3df71845e1211994008602b
Wladislav ヴラド Artsimovich <git@frost.kiwi>
2023-10-31 14:00:03 +0900

    Add gradient to the spots

--- src/shd/border.fs
+++ src/shd/border.fs
@@ -10,11 +10,18 @@ void main()
 {
 	vec3 blackColor = vec3(0.0);
 
+	/* Cricle SDF, minus pixel size */
 	float dist = length(vtx_fs) - (1.0 - pxsize);
 
+	/* Using reciprocal to avoid division */
 	float smoothedAlpha_black = dist * pxsize_rcp;
+	/* Need to clamp, but only one direciton, so using max. Logically it should
+	   be min, but the SDF works the other way around. */
 	float smoothedAlpha_color = max((dist + 0.2) * pxsize_rcp, 0.0);
 
-	vec3 finalColor = mix(blackColor, color, 1.0 - smoothedAlpha_color);
+	/* Add a slight gradient */
+	vec3 gradient = color * ((vtx_fs.y + 1.5) * 0.5);
+
+	vec3 finalColor = mix(blackColor, gradient, 1.0 - smoothedAlpha_color);
 	gl_FragColor = vec4(finalColor, alpha * (1.0 - smoothedAlpha_black));
 }
